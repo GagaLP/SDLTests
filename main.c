949adf5c81970c91179b18f89ba33277fa8c2f89
GagaLP <GabrielMitterrutzner@gmail.com>
2017-04-14 23:11:34 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main() {
+    char array[4] = {'e', 'a', 'i', 'o'};
+    printf("Hello, World!\n");
+    printf("%c", 2[array]);
+
+    return 0;
+}
